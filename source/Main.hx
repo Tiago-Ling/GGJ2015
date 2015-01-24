52a330dacde58b4c442975aa5994efe89c50e9ea
Luiz F. A. de Prá <rawarkanis@gmail.com>
2015-01-24 12:26:45 -0200

    Config base2

--- source/Main.hx
+++ source/Main.hx
@@ -8,7 +8,7 @@ import flash.Lib;
 import flixel.FlxGame;
 import flixel.FlxState;
 
-class Main extends Sprite 
+class Main extends Sprite
 {
 	var gameWidth:Int = 1024; // Width of the game in pixels (might be less / more in actual pixels depending on your zoom).
 	var gameHeight:Int = 576; // Height of the game in pixels (might be less / more in actual pixels depending on your zoom).
@@ -17,40 +17,43 @@ class Main extends Sprite
 	var framerate:Int = 60; // How many frames per second the game should run at.
 	var skipSplash:Bool = false; // Whether to skip the flixel splash screen that appears in release mode.
 	var startFullscreen:Bool = false; // Whether to start the game in fullscreen on desktop targets
-	
+
 	// You can pretty much ignore everything from here on - your code should go in your states.
-	
+
 	public static function main():Void
-	{	
+	{
 		Lib.current.addChild(new Main());
 	}
-	
-	public function new() 
+
+	public function new()
 	{
 		super();
-		
-		if (stage != null) 
+
+		if (stage != null)
 		{
 			init();
 		}
-		else 
+		else
 		{
 			addEventListener(Event.ADDED_TO_STAGE, init);
 		}
 	}
-	
-	private function init(?E:Event):Void 
+
+	private function init(?E:Event):Void
 	{
 		if (hasEventListener(Event.ADDED_TO_STAGE))
 		{
 			removeEventListener(Event.ADDED_TO_STAGE, init);
 		}
-		
+
 		setupGame();
 	}
-	
+
 	private function setupGame():Void
 	{
+		var config = new Configuration();
+		config.load(AssetPaths.config__xml);
+
 		var stageWidth:Int = Lib.current.stage.stageWidth;
 		var stageHeight:Int = Lib.current.stage.stageHeight;
 
